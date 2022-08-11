e433306802ef9b721816674759fb618af106113b
AP <ashis.n.patel@gmail.com>
2022-08-10 20:57:11 -0500

    Added gitignore

---
+++ models/Activity.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose'); 
+
+const ActivitySchema = new Schema({
+    activityName: {
+        type: String, 
+        required: true,
+        minLength: 1
+    },
+    activityCoefficient: {
+        type: Number,
+        required: true,
+        default: 1
+    }
+});
+
+const Activity = model('Activity', ActivitySchema); 
+
+module.exports = Thought; 
+
